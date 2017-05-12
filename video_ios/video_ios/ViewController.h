72672002c7fa0269bf87d3f20f73e66cead30fc7
wuming114 <1402370971@qq.com>
2017-05-12 17:16:33 +0800

    init commit

---
+++ video_ios/video_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video_ios
+//
+//  Created by wim on 2017/5/12.
+//  Copyright © 2017年 wim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
